1af161c158e1ed3d1f428546af7b36a9d5d37dfb
Eric Siegel <siegel.eric@gmail.com>
2013-02-19 22:02:45 -0800

    hw1 using blocks 8x8

--- HW1/student/student_func.cu
+++ HW1/student/student_func.cu
@@ -33,24 +33,26 @@
 
 #include "reference_calc.cpp"
 #include "utils.h"
+#include <stdio.h>
 
 __global__
 void rgba_to_greyscale(const uchar4* const rgbaImage,
                        unsigned char* const greyImage,
-                       int numRows, int numCols)
-{
-  //TODO
-  //Fill in the kernel to convert from color to greyscale
-  //the mapping from components of a uchar4 to RGBA is:
-  // .x -> R ; .y -> G ; .z -> B ; .w -> A
-  //
-  //The output (greyImage) at each pixel should be the result of
-  //applying the formula: output = .299f * R + .587f * G + .114f * B;
-  //Note: We will be ignoring the alpha channel for this conversion
+                       int numRows, int numCols) {
 
-  //First create a mapping from the 2D block and grid locations
-  //to an absolute 2D location in the image, then use that to
-  //calculate a 1D offset
+  int row = blockDim.x * blockIdx.x + threadIdx.x;
+  int col = blockDim.y * blockIdx.y + threadIdx.y;
+
+  if (row >= numRows || col >= numCols) {
+     return;
+  }
+  
+  int offset = (row * numCols) + col;
+
+  uchar4 value = rgbaImage[offset];
+  greyImage[offset] = .299f * value.x + 
+                      .587f * value.y + 
+                      .114f * value.z;
 }
 
 void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
@@ -58,11 +60,16 @@ void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_r
 {
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
-  const dim3 blockSize(1, 1, 1);  //TODO
-  const dim3 gridSize( 1, 1, 1);  //TODO
+  const dim3 blockSize(8, 8, 1);
+  /*const dim3 gridSize( (numRows / 8) + 1 , (numCols / 8) + 1, 1);*/
+  const dim3 gridSize(40, 70, 1);
+
+  printf("numrows x: %zu numcols  y: %zu\n", numRows, numCols);
+  printf("grid x: %u grid y: %u\n", (numRows / 8) + 1 , (numCols / 8) + 1);
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
-  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+  cudaDeviceSynchronize();
+  checkCudaErrors(cudaGetLastError());
 
   /****************************************************************************
   * You can use the code below to help with debugging, but make sure to       *
